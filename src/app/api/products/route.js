ca8887a342f690321a48d736c3c1f87fa09a9cd9
Kasun Lakmal <kasunlakmal20487ks1@gmail.com>
2026-06-16 21:31:29 +0530

    feat: add products API route and disable build-time linting and type checks

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
+
+export const dynamic = "force-dynamic";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
