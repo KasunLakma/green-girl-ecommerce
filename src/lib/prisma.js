c48d74185fac5877410bd6bfe93365cb0adf1476
Kasun Lakmal <kasunlakmal20487ks1@gmail.com>
2026-06-16 21:49:31 +0530

    feat: initialize Prisma client and implement products API routes for fetch and creation

---
+++ src/lib/prisma.js
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const globalForPrisma = global;
+let prisma;
+
+if (globalForPrisma.prisma) {
+  prisma = globalForPrisma.prisma;
+} else {
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+  const adapter = new PrismaPg(pool);
+  prisma = new PrismaClient({ adapter });
+  if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
+  }
+}
+
+export { prisma };
